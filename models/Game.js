d5200b2398ee87ea698b920c7d0aacb237a4bd34
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-15 18:03:28 +0700

    remodel Game model to have players info & state

--- models/Game.js
+++ models/Game.js
@@ -4,6 +4,12 @@ const gameSchema = mongoose.Schema({
   fen: String,
   pgn: String,
   history: [String],
+  state: String,
+  pwhite: String,
+  pblack: String,
+  user0: String,
+  user1: String,
+  spectators: [String],
 });
 
 module.exports = mongoose.model("Game", gameSchema);
