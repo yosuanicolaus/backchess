a43d63ee92d9c2c0d0a74fd6ea7db90a8ad86270
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-16 07:35:53 +0700

    automatically exit script after done

--- scripts/deleteAll.js
+++ scripts/deleteAll.js
@@ -3,12 +3,12 @@ require("dotenv").config();
 const Game = require("../models/Game");
 const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI;
-console.log(uri);
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     Game.deleteMany().then(() => {
       console.log("deleted all games");
+      process.exit();
     });
   })
   .catch((err) => {
