c0c3b5ed4d61083a5a619ae9bf854764a17c2b19
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-18 09:14:30 +0700

    add /user/new and /user/get in user router

--- routes/user.js
+++ routes/user.js
@@ -7,24 +7,36 @@ const router = express.Router();
 router.get("/test", async (req, res) => {
   try {
     console.log("received /user/test");
-    const newUser = new User({
-      uid: "asdfjqweru2354167",
-      email: "niceemail@logichess.com",
-    });
-    await newUser.save();
-    res.json(newUser);
+    const user = await User.findById("qwertywarrior12345678");
+    if (!user) return res.status(404).json("user not found");
+    res.json(user);
   } catch (error) {
-    res.status(400).json(error);
+    res.status(400).json(error.message);
   }
 });
 
 // create new User
 router.post("/new", async (req, res) => {
+  const { uid } = req.body;
+  if (!uid) return res.status(400).json("missing {uid}");
+
   try {
-    const { user } = req.body;
-    const newUser = createUser(user);
+    const newUser = new User({ uid });
     await newUser.save();
-    res.json({ newUser });
+    res.json(newUser);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+// get user by UID
+router.get("/:uid", async (req, res) => {
+  const { uid } = req.params;
+
+  try {
+    const user = await User.findById(uid);
+    if (!user) return res.status(404).json("user not found");
+    res.json(user);
   } catch (error) {
     res.status(400).json(error.message);
   }
