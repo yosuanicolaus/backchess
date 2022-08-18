20276613a9a18bf43b567cf6e14de846740865d8
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-18 12:58:20 +0700

    post new message to chat room

--- routes/chat.js
+++ routes/chat.js
@@ -52,4 +52,23 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// post new message to chat room
+router.post("/:id/new-message", async (req, res) => {
+  const { id } = req.params;
+  const { text, username, uid } = req.body;
+
+  try {
+    const chat = await Chat.findById(id);
+    if (!chat) throw "404/chat not found";
+
+    const message = new Message({ text, username, uid });
+    chat.messages.push(message);
+
+    await chat.save();
+    res.json(chat);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
+
 module.exports = router;
