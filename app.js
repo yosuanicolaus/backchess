fef38304beffc9607a64233219ed3e5398eeb2ca
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-18 10:14:37 +0700

    create /chat/message/new route & update test

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const Game = require("./models/Game");
 const User = require("./models/User");
+const Chat = require("./models/Chat");
+const Message = require("./models/Message");
 const gameRouter = require("./routes/game");
 const userRouter = require("./routes/user");
 const chatRouter = require("./routes/chat");
@@ -12,11 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// returns all games and users in db
+// returns all games, users, chats, and messages in db
 app.get("/", async (req, res) => {
   const games = await Game.find();
   const users = await User.find();
-  res.json({ games, users });
+  const chats = await Chat.find();
+  const messages = await Message.find();
+  res.json({ games, users, chats, messages });
 });
 
 app.get("/test", (req, res) => {
--- routes/chat.js
+++ routes/chat.js
@@ -7,4 +7,18 @@ router.get("/test", (req, res) => {
   res.json({ test: "/chat/test successful" });
 });
 
+router.post("/message/new", async (req, res) => {
+  const { text, uid } = req.body;
+  if (!text) return res.status(400).json("missing {text}");
+  if (!uid) return res.status(400).json("missing {uid}");
+
+  try {
+    const newMessage = new Message({ text, user: uid });
+    await newMessage.save();
+    res.json(newMessage);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
 module.exports = router;
