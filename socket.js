1a53d964f8e91639066da18a159ca69d76631b3d
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-19 15:18:11 +0700

    rewrite socket 'join' & 'leave' listener

--- socket.js
+++ socket.js
@@ -1,5 +1,6 @@
 const Chess = require("./logichess/index");
 const Game = require("./models/Game");
+const User = require("./models/User");
 
 const express = require("express");
 const app = express();
@@ -11,15 +12,59 @@ const io = require("socket.io")(server, {
 io.on("connection", (socket) => {
   console.log("a user just connected");
 
-  socket.on("join", async (gameID) => {
-    socket.join(gameID);
-    const game = await Game.findById(gameID);
-    io.to(gameID).emit("update-game", game);
+  socket.on("join", async ({ id, uid }) => {
+    try {
+      socket.join(id);
+      const game = await Game.findById(id);
+      if (!game) throw "game not found";
+      const user = await User.findById(uid);
+      if (!user) throw "user not found";
+
+      await game.joinUser(user);
+      io.to(id).emit("update-game", game);
+    } catch (error) {
+      emitError(io, id, error);
+    }
   });
 
-  socket.on("disconnect", () => {
+  socket.on("leave", async ({ id, uid }) => {
+    try {
+      socket.leave(id);
+      const game = await Game.findById(id);
+      if (!game) throw "game not found";
+
+      if (game.state === "waiting") {
+        if (game.user0.uid !== uid) {
+          throw "403/user is not in the game";
+        }
+        // game is empty
+        await game.delete();
+        return res.json({ success: `game ${id} deleted` });
+      }
+
+      await game.leaveUid(uid);
+      io.to(id).emit("update-game", game);
+    } catch (error) {
+      emitError(io, id, error);
+    }
+  });
+
+  socket.on("disconnecting", () => {
+    const rooms = socket.rooms.slice();
+    rooms.forEach((room) => {
+      io.to(room).emit("info", "a user is disconnecting");
+    });
+
     console.log("a user just disconnected");
   });
 });
+
+function emitError(io, room, error) {
+  if (typeof error === "string") {
+    io.to(room).emit("error", error);
+  } else {
+    io.to(room).emit("error", error.message);
+  }
+}
 
 module.exports = server;
