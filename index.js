fa3ba148ab619db06f8d2000eece7f70142572e7
Yosua Nicolaus <cryodawn257@gmail.com>
2022-08-24 13:49:54 +0700

    prepare to deploy to heroku, separate start & dev

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const apiServer = require("./app");
 const socketServer = require("./socket");
 
+const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI;
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    apiServer.listen(3001, () => {
-      console.log("API server listening on port 3001");
+    apiServer.listen(port, () => {
+      console.log(`API server listening on port ${port}`);
     });
 
-    socketServer.listen(3003, () => {
-      console.log("SocketIO server listening on port 3003");
+    socketServer.listen(port, () => {
+      console.log(`SocketIO server listening on port ${port}`);
     });
   })
   .catch((err) => {
